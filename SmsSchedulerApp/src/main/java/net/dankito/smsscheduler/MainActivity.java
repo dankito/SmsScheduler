a46b94cb177d61c77944bcf96e7eeed6fad6c1fd
dankito <dankito@user.noreply.github.com>
2017-01-28 20:34:15 +0100

    Implemented picking receiver's phone number from Contacts

--- SmsSchedulerApp/src/main/java/net/dankito/smsscheduler/MainActivity.java
+++ SmsSchedulerApp/src/main/java/net/dankito/smsscheduler/MainActivity.java
@@ -1,6 +1,12 @@
 package net.dankito.smsscheduler;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -9,6 +15,9 @@ import android.widget.TextView;
 
 import net.dankito.android.util.controls.DateTimePicker;
 import net.dankito.android.util.controls.DateTimePickerCallback;
+import net.dankito.android.util.services.IPermissionsManager;
+import net.dankito.android.util.services.PermissionRequestCallback;
+import net.dankito.android.util.services.PermissionsManager;
 import net.dankito.smsscheduler.services.ScheduledSms;
 import net.dankito.smsscheduler.services.ScheduledSmsManager;
 
@@ -18,10 +27,14 @@ import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
+  protected static final int PICK_CONTACT_REQUEST_ID = 27388;
+
   protected static final DateFormat EXECUTE_AT_DATE_FORMAT = new SimpleDateFormat("dd.MM.yyyy HH:mm");
 
 
   protected ScheduledSmsManager scheduledSmsManager;
+
+  protected IPermissionsManager permissionsManager;
 
 
   protected TextView txtvwExecuteAt;
@@ -43,6 +56,8 @@ public class MainActivity extends AppCompatActivity {
 
   protected void setupDependencies() {
     scheduledSmsManager = new ScheduledSmsManager(this);
+
+    permissionsManager = new PermissionsManager(this);
   }
 
   protected void setupUi() {
@@ -53,6 +68,9 @@ public class MainActivity extends AppCompatActivity {
     setExecuteAtToInitialValue();
 
     edtxReceiverPhoneNumber = (EditText)findViewById(R.id.edtxReceiverPhoneNumber);
+
+    Button btnPickReceiverPhoneNumberFromContacts = (Button)findViewById(R.id.btnPickReceiverPhoneNumberFromContacts);
+    btnPickReceiverPhoneNumberFromContacts.setOnClickListener(btnPickReceiverPhoneNumberFromContactsClickListener);
 
     edtxMessageText = (EditText)findViewById(R.id.edtxMessageText);
 
@@ -75,6 +93,53 @@ public class MainActivity extends AppCompatActivity {
     txtvwExecuteAt.setTag(executeAt);
   }
 
+
+  @Override
+  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+
+    switch(requestCode) {
+      case PICK_CONTACT_REQUEST_ID:
+        if (resultCode == Activity.RESULT_OK) {
+          getPhoneNumberFromSelectedContact(data);
+        }
+        break;
+    }
+  }
+
+  protected void getPhoneNumberFromSelectedContact(Intent data) {
+    Uri contactData = data.getData();
+    Cursor contactCursor = getContentResolver().query(contactData, null, null, null, null);
+    if (contactCursor.moveToFirst()) {
+      String id = contactCursor.getString(contactCursor.getColumnIndexOrThrow(ContactsContract.Contacts._ID));
+
+      int hasPhone = contactCursor.getInt(contactCursor.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER));
+      if (hasPhone > 0) {
+        Cursor phones = getContentResolver().query(
+            ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,
+            ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
+            new String[] { id }, null);
+
+        if(phones.moveToFirst()) {
+          String phoneNumber = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+          edtxReceiverPhoneNumber.setText(phoneNumber);
+        }
+      }
+    }
+  }
+
+
+  protected void pickReceiverFromContacts() {
+    permissionsManager.checkPermission(Manifest.permission.READ_CONTACTS, R.string.rationale_read_contacts, new PermissionRequestCallback() {
+      @Override
+      public void permissionCheckDone(String permission, boolean isGranted) {
+        if(isGranted) {
+          Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+          startActivityForResult(intent, PICK_CONTACT_REQUEST_ID);
+        }
+      }
+    });
+  }
 
   protected void scheduleSms() {
     Calendar executeAt = (Calendar)txtvwExecuteAt.getTag();
@@ -99,6 +164,13 @@ public class MainActivity extends AppCompatActivity {
           }
         }
       });
+    }
+  };
+
+  protected View.OnClickListener btnPickReceiverPhoneNumberFromContactsClickListener = new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+      pickReceiverFromContacts();
     }
   };
 
