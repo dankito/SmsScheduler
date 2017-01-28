0cafe934eaf786003b38162763f3353e8b798840
dankito <dankito@user.noreply.github.com>
2017-01-28 16:43:00 +0100

    Handling controls to send sms

--- SmsSchedulerApp/src/main/java/net/dankito/smsscheduler/MainActivity.java
+++ SmsSchedulerApp/src/main/java/net/dankito/smsscheduler/MainActivity.java
@@ -1,7 +1,10 @@
 package net.dankito.smsscheduler;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import net.dankito.android.util.services.AlarmManagerCronService;
 import net.dankito.android.util.services.ICronService;
@@ -13,6 +16,11 @@ public class MainActivity extends AppCompatActivity {
   protected ICronService cronService;
 
   protected SmsService smsService;
+
+
+  protected EditText edtxReceiverPhoneNumber;
+
+  protected EditText edtxMessageText;
 
 
   @Override
@@ -32,6 +40,29 @@ public class MainActivity extends AppCompatActivity {
 
   protected void setupUi() {
     setContentView(R.layout.activity_main);
+
+    edtxReceiverPhoneNumber = (EditText)findViewById(R.id.edtxReceiverPhoneNumber);
+
+    edtxMessageText = (EditText)findViewById(R.id.edtxMessageText);
+
+    Button btnScheduleSms = (Button)findViewById(R.id.btnScheduleSms);
+    btnScheduleSms.setOnClickListener(btnScheduleSmsClickListener);
   }
+
+
+  protected void scheduleSms() {
+    String receiverPhoneNumber = edtxReceiverPhoneNumber.getText().toString();
+    String messageText = edtxMessageText.getText().toString();
+
+    smsService.sendTextSms(receiverPhoneNumber, messageText);
+  }
+
+
+  protected View.OnClickListener btnScheduleSmsClickListener = new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+      scheduleSms();
+    }
+  };
 
 }
