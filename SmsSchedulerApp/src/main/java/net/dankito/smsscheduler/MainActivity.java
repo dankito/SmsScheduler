b48bf1c21fb0d820bf413b8869d3745d29eabbc6
dankito <dankito@user.noreply.github.com>
2017-01-28 16:15:19 +0100

    Added smsService

--- SmsSchedulerApp/src/main/java/net/dankito/smsscheduler/MainActivity.java
+++ SmsSchedulerApp/src/main/java/net/dankito/smsscheduler/MainActivity.java
@@ -5,11 +5,14 @@ import android.os.Bundle;
 
 import net.dankito.android.util.services.AlarmManagerCronService;
 import net.dankito.android.util.services.ICronService;
+import net.dankito.android.util.services.SmsService;
 
 public class MainActivity extends AppCompatActivity {
 
 
   protected ICronService cronService;
+
+  protected SmsService smsService;
 
 
   @Override
@@ -24,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
 
   protected void setupDependencies() {
     cronService = new AlarmManagerCronService(this);
+    smsService = new SmsService();
   }
 
   protected void setupUi() {
